283cceadb67242518e81a1c3a83150e2583a4d76
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-12-30 18:30:49 +0100

    fixed

--- kochsnowflake.py
+++ kochsnowflake.py
@@ -1,37 +1,36 @@
 import turtle
 
-wn = turtle.Screen()
-wn.setup(1200, 1200)
-wn.bgcolor("black")
 
-myTurtle = turtle.Turtle()
+def kochOneSide(myTurtle, level, size):
+    if level == 0:
+        myTurtle.forward(size)
+    else:
+        for angle in [60, -120, 60, 0]:
+            kochOneSide(myTurtle, level - 1, size / 3)
+            myTurtle.left(angle)
 
+def kochSnowFlake(level, size=300):
 
-def kochOneSide(turtle, length, depth):
-    if depth == 0:
-        turtle.forward(length)
-        return
-    kochOneSide(turtle, length / 3, depth - 1)
-    turtle.right(60)
-    kochOneSide(turtle, length / 3, depth - 1)
-    turtle.left(120)
-    kochOneSide(turtle, length / 3, depth - 1)
-    turtle.right(60)
-    kochOneSide(turtle, length / 3, depth - 1)
+    wn = turtle.Screen()
+    wn.bgcolor("black")
+    wn.setup(600, 600)
 
-
-def kochSnowFlake(myTurtle, length, depth):
+    myTurtle = turtle.Turtle()
+    myTurtle.speed(0)
     myTurtle.penup()
-    myTurtle.setposition(-length / 2, 0)
+    myTurtle.goto(-size / 2, 0)
     myTurtle.pendown()
-    for i in range(3):
-        kochOneSide(myTurtle, length, depth)
-        myTurtle.left(120)
+    myTurtle.pencolor("green")
+
+    for _ in range(3):
+        kochOneSide(myTurtle, level, size)
+        myTurtle.right(120)
+
+    wn.mainloop()
 
 
-myTurtle.pencolor("green")
-myTurtle.speed(0)
-kochSnowFlake(myTurtle, 600, 4)
-
-
-wn.exitonclick()
+try:
+    level = int(input("Enter Koch Snowflake level: "))
+    kochSnowFlake(level)
+except ValueError:
+    print('Enter positive number!')
